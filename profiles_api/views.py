fdb73cbb0d635216056f43a22052e7d054a981b1
Abhishek Kumar <akumar3@cs.iitr.ac.in>
2021-06-20 03:49:53 +0530

    added basic http method api end points

--- profiles_api/views.py
+++ profiles_api/views.py
@@ -1,11 +1,17 @@
 from rest_framework.views import APIView
 from rest_framework.response import Response
+from rest_framework import status
+
+
+from profiles_api import serializers
 
 # Create your views here.
 
 
 class HelloAPIView(APIView):
     """Test API View"""
+    serializer_class = serializers.HelloSerializer
+
 
 
     def get(self, request, format=None):
@@ -18,5 +24,42 @@ class HelloAPIView(APIView):
         ]
 
         return Response({'message':'Hello', 'an_apiview': an_apiview})
+
+
+    def post(self, request):
+        """Create a hello message with your name"""
+        serializer = self.serializer_class(data=request.data)
+        
+
+        if serializer.is_valid():
+            name = serializer.validated_data.get('name')
+            message = f'Hello {name}'
+            return Response({'message':message})
+        else:
+            return Response(
+                serializer.errors,
+                status = status.HTTP_400_BAD_REQUEST
+                )
+
+
+    def put(self, request, pk=None):
+        """Handle updating an object"""
+        #pk stands for primary key
+        serialzer = self.serializer_class(data=request.data)
+        return Response({'method':'PUT'})
+
+
+    def patch(self, request, pk=None):
+        """Handle a partial update of an object"""
+        serializer = self.serializer_class(data=request.data)
+        return Response({'method':'PATCH'})
+
+
+    def delete(self, request,pk=None):
+        """Handle delete of an object"""
+        serializer = self.serializer_class(data=request.data)
+        return Response({'method':'DELETE'})
+
+
 
 
